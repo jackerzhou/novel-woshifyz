468e017dc1b8d6e42eb73db67b49270ac7a396f8
fyz <zhouzhen912@gmail.com>
2012-11-22 12:59:29 +0800

    fix

--- diy/update.py
+++ diy/update.py
@@ -9,28 +9,36 @@ import re
 import os,os.path
 from utils import gen_wdqk_path
 
-def get_content(qu):
+def get_content(conn,qu):
     while True:
         if qu.empty():
             print 'over'
             break
-        url = qu.get()
+        tmp = qu.get()
+        url = tmp['href']
         try:
             num = re.match(r'http://www.saesky.net/wudongqiankun/(\S+?)\.html',url).group(1)
             file_name = 'wdqk_%s.txt' % (num,)
             if os.path.exists(file_name):
                 continue
-            content = urllib.urlopen(url).read()
-            c_soup = BeautifulSoup(content)
-            content_body = c_soup.find('div',{'class':'content-body'})
-            script_garbage = content_body.findAll('script')
-            [t.extract() for t in script_garbage]
-            div_garbage = content_body.findAll('div',{'style':re.compile('.+')})
-            [t.extract() for t in div_garbage]
-            con_str = str(content_body)
+            while True:
+                try:
+                    content = urllib.urlopen(url).read()
+                    c_soup = BeautifulSoup(content)
+                    content_body = c_soup.find('div',{'class':'content-body'})
+                    script_garbage = content_body.findAll('script')
+                    [t.extract() for t in script_garbage]
+                    div_garbage = content_body.findAll('div',{'style':re.compile('.+')})
+                    [t.extract() for t in div_garbage]
+                    con_str = str(content_body)
+                    if con_str:
+                        break
+                except:
+                    print url
             fp = file(file_name,'w')
             fp.write(con_str)
             fp.close()
+            conn.execute("insert into wdqk (num,name) values(%s,%s)",tmp['href'].encode('utf-8'),tmp['name'].encode('utf-8'))
         except Exception,e:
             print url
             
@@ -54,8 +62,7 @@ def main():
                 tmp = {}
                 tmp['href'] = p.a['href']
                 tmp['name'] = p.string
-                conn.execute("insert into wdqk (num,name) values(%s,%s)",tmp['href'].encode('utf-8'),tmp['name'].encode('utf-8'))
-                qu.put(tmp['href'])
+                qu.put(tmp)
             except:
                 pass
         else:
@@ -66,7 +73,8 @@ def main():
     if not os.path.exists(path):
         os.mkdir(path)
     os.chdir(path)
-    get_content(qu)
+    get_content(conn,qu)
+    conn.close()
 
 if __name__ == '__main__':
     main()
